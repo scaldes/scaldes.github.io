f819e783b8df85ae43e4c6276f890d1de95a4372
scaldes <cal16016@byui.edu>
2016-11-05 19:31:58 -0600

    Update
    Changed layout and trying to add modularization.

---
+++ js_test/scripts/module.js
@@ -0,0 +1,7 @@
+//jQuery function to implement modularization
+
+$(function() {
+    $("#page-header").load("../js_test/modules/header.html");
+    $("#page-nav").load("../js_test/modules/nav.html");
+    $("#page-footer").load("../js_test/modules/footer.html");
+});

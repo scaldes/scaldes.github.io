4f8fbbf18e5d43c87bed548d5f5af3e45ca0b3e0
scaldes <cal16016@byui.edu>
2016-11-28 10:42:33 -0700

    update

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -25,6 +25,7 @@ $(function () {
             url: '//api.wunderground.com/api/ea19bcd1f5d58aa1/geolookup/conditions/q/' + lat + ',' + long + '.json'
             , dataType: "jsonp"
             , success: function (data) {
+                console.log(data);
                 var cityState = data.location.city + ", " + data.location.state;
                 var temp = Math.round(data.current_observation.temp_f);
                 var currentConditions = (data.current_observation.weather);

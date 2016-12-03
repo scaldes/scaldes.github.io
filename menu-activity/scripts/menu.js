2c2e5e9e433e89cae48418bdc525cb1d9d1bb2b4
scaldes <cal16016@byui.edu>
2016-12-03 16:49:21 -0700

    Update
    Trying to implement the search bar assessment. Still working, just
    updating to be on site.

---
+++ menu-activity/scripts/menu.js
@@ -0,0 +1,53 @@
+// Get weather data from wunderground.com
+function getData(input) {
+   var pageNavigation = document.getElementById('page-nav');
+    pageNavigation.addEventListener('click', pageNavigation);
+   $( "a" ).click(function( event ) {
+  event.preventDefault();
+  $( "<div>" )
+    .append( "default " + event.type + " prevented" )
+    .appendTo( "#log" );
+});
+     // Get the data from the wunderground API
+    $.ajax({
+        url: "http://api.wunderground.com/api/ea19bcd1f5d58aa1/geolookup/conditions/q/" + input + ".json"
+        , dataType: "jsonp"
+        , success: function (data) {
+            console.log(data);
+            var location = data.location.city + ', ' + data.location.state;
+            var temp_f = data.current_observation.temp_f;
+            console.log('Location is: ' + location);
+            console.log('Temp is: ' + temp_f);
+            $("#cityDisplay").text(location);
+            $("title").html(location + " | Weather Center");
+            $("#currentTemp").html(Math.round(temp_f) + '°');
+            $("#summary").text(toTitleCase(data.current_observation.icon));
+            $("#cover").fadeOut(250);
+        }
+    });
+}
+    // Intercept the menu link clicks
+    $("#page-nav").on("click", "a", function (evt) {
+        evt.preventDefault();
+        // With the text value get the needed value from the weather.json file
+        var jsonCity = $(this).text(); // Franklin, etc...
+        console.log(jsonCity);
+        $.ajax({
+            url: "/menu-activity/scripts/weather.json"
+            , dataType: "json"
+            , success: function (data) {
+                console.log(data);
+                console.log(data[jsonCity]);
+                var zip = data[jsonCity].zip;
+                console.log(zip);
+                getData(zip);
+            }
+        });
+    });
+   
+// A function for changing a string to TitleCase
+function toTitleCase(str) {
+    return str.replace(/\w+/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
--- search/search.js
+++ search/search.js
@@ -1,24 +1,29 @@
-// Current Location Scripts
-$(function () {
-    //will identify status
-    var status = $('#status');
-    //dont touch, fully operational ("find out where youre at")
-    (function getGeoLocation() {
-        status.text('Getting Location...');
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                //will send to the getData function
-                var lat = position.coords.latitude;
-                var long = position.coords.longitude;
-                // Call the getData function, send the lat and long
-                getData(lat, long);
-            });
-        }
-        else {
-            status.text("Your browser doesn't support Geolocation or it is not enabled!");
-        }
-    })();
-    // Get the data from the wunderground API
+$('#query').keyup(function(){
+  // All code will be inside of this block
+  
+    var value = $('#query').val();
+    var rExp = new RegExp(value, "i");
+    
+
+ $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+      // Intercept the menu link clicks
+  $("searchResults").on("click", "a", function(evt) {
+    evt.preventDefault();
+  console.log(data);
+     // Begin building output
+    var output = '<ol>';
+    $.each(data.RESULTS, function(key, val) {
+      if (val.name.search(rExp) != -1) {
+        output += '<li>';
+        output += '<a href="http://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+        output += '</li>';
+      }
+    }); // end each
+    output += '</ol>';
+    $("#searchResults").html(output); // send results to the page
+  }); // end getJSON
+    }); // end keyup
+// Get the data from the wunderground API
     function getData(lat, long) {
         //PUT CODE HERE
         $.ajax({
@@ -56,3 +61,21 @@ $(function () {
         });
     }
 });
+// Intercept the menu link clicks
+    $("#page-nav").on("click", "a", function (evt) {
+        evt.preventDefault();
+        // With the text value get the needed value from the weather.json file
+        var jsonCity = $(this).text(); // Franklin, etc...
+        console.log(jsonCity);
+        $.ajax({
+            url: "/menu-activity/scripts/weather.json"
+            , dataType: "json"
+            , success: function (data) {
+                console.log(data);
+                console.log(data[jsonCity]);
+                var zip = data[jsonCity].zip;
+                console.log(zip);
+                getData(zip);
+            }
+        });
+    });
